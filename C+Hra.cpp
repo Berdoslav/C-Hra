fd4415f57bdd16b8fffa38cc70e307dc9b1026c6
Bernard <nodashcz@gmail.com>
2026-05-18 17:15:00 +0200

    Rework Class vybírání
    vylepšení a optimalizace vybírání klás. Přidal jsem všechny informace o postavě a taky možnost si klasu změnit

--- C+Hra.cpp
+++ C+Hra.cpp
@@ -6,46 +6,119 @@ using namespace std;
 
 int main() {
 
-int classa, hp, silaU;
-bool paladin, mage;
+    srand(time(0));
 
-paladin = false;
-mage = false;
+    int classa, maxHp, hp, maxMana, mana,
+        utok, gold = 0, level = 1, exp = 0;
 
-cout << "Vitej ve hre jaka classa si ? ";
-cin >> classa;
-
-switch(classa) {
-
-case 1:
-    cout << "Vitej BOJOVNIKU! ";
-    hp = 100;
-    silaU = 10;
-    break;
-
-case 2:
-    cout << "Vitej KOUZELNIKU! ";
-    hp = 65;
-    silaU = 15;
-    mage = true;
-    break;
-
-case 3:
-    cout << "Vitej PALADINE! ";
-    hp = 140;
-    silaU = 6;
-    paladin = true;
-    break;
-
-case 4:
-    cout << "Vitej LUCISTNIKU!";
-    hp = 80;
-    silaU = 12;
-    break;
-
-default:
-    cout << "Neplatna classa!";
-}
+    string jmenoClassy;
+    string typEnergie;
 
 
+
+    bool potvrzeni = false;
+
+    while(potvrzeni == false) {
+
+        cout << "==========================" << endl;
+        cout << "1 - Bojovnik" << endl;
+        cout << "2 - Kouzelnik" << endl;
+        cout << "3 - Paladin" << endl;
+        cout << "4 - Lucistnik" << endl;
+        cout << "==========================" << endl;
+
+        cout << "Vyber classu: ";
+        cin >> classa;
+
+        switch(classa) {
+
+        case 1:
+            jmenoClassy = "Bojovnik";
+
+            maxHp = 120;
+            hp = maxHp;
+
+            typEnergie = "Energie";
+            maxMana = 50;
+            mana = maxMana;
+
+            utok = 10;
+            break;
+
+        case 2:
+            jmenoClassy = "Kouzelnik";
+
+            maxHp = 70;
+            hp = maxHp;
+
+            typEnergie = "Mana";
+            maxMana = 100;
+            mana = maxMana;
+
+            utok = 15;
+            break;
+
+        case 3:
+            jmenoClassy = "Paladin";
+
+            maxHp = 140;
+            hp = maxHp;
+
+            typEnergie = "Mana";
+            maxMana = 50;
+            mana = maxMana;
+
+            utok = 7;
+            break;
+
+        case 4:
+            jmenoClassy = "Lucistnik";
+
+            maxHp = 90;
+            hp = maxHp;
+
+            typEnergie = "Energie";
+            maxMana = 60;
+            mana = maxMana;
+
+            utok = 12;
+            break;
+
+        default:
+            cout << "Neplatna classa!" << endl;
+            continue;
+        }
+
+        cout << endl;
+        cout << "===== TVOJE CLASSA =====" << endl;
+
+        cout << "Classa: " << jmenoClassy << endl;
+
+        cout << "HP: " << hp << "/" << maxHp << endl;
+
+        cout << typEnergie << ": "
+             << mana << "/" << maxMana << endl;
+
+        cout << "Utok: " << utok << endl;
+
+        cout << "Level: " << level << endl;
+        cout << "Gold: " << gold << endl;
+        cout << "EXP: " << exp << endl;
+
+        char odpoved;
+
+        cout << endl;
+        cout << "Chces potvrdit classu? (a/n): ";
+        cin >> odpoved;
+
+        if(odpoved == 'a') {
+            potvrzeni = true;
+        }
+
+        cout << endl;
+    }
+
+    cout << "Hra zacina..." << endl;
+
+    return 0;
 }
